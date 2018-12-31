e02b09206fd448c6a07e4911d7879752825493f0
haribol aaron <aaron@adubsvc.net>
2018-12-30 20:46:02 -0700

    Comment out debugging lines, add comment for debug help.

--- bin/mouse-natural-scroll.sh
+++ bin/mouse-natural-scroll.sh
@@ -2,11 +2,14 @@
 
 # Enable "natural scrolling" (reverse mouse wheel) in Linux
 
+# to debug
+# set -x
+
 # get the mouseid by filtering output of xinput --list
 # matches id of first device with word "mouse"
 mouseid=$(xinput --list | sed -E '/mouse/I!d;s/.*?mouse.*?id=([0-9]+).*/\1/i;q')
 
-echo $mouseid
+#echo $mouseid
 
 # set natural scrolling property to given mouseid
 xinput --set-prop $mouseid 'libinput Natural Scrolling Enabled' 1
